9f3322c731be63f3a7da00fe1a57f6274d294227
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-05 13:48:10 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 /**
- *main - prints "Programming is like building a multiligual pizzle"
+ *main - prints line
  *
  *Return: 0 if successful
  */
 int main(void) 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
