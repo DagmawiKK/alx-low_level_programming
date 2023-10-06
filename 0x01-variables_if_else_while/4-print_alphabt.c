4a57412556f8d4b5e134127c4bd34e59568016ab
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-06 14:56:43 +0300

    solutions for 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,24 @@
 int main(void)
 {
 	char a;
+	char e;
+	char q;
 
 	a = 'a';
+	e = 'e';
+	q = 'q';
 	while (a <= 'z')
 	{
-		if (a != 'e' || a != 'q')
+		if (a == e || a == q)
+		{
+			a += 1;
+			continue;
+		}
+		else
+		{
 			putchar(a);
-		a += 1;
+			a += 1;
+		}
 	}
 	putchar('\n');
 	return (0);
