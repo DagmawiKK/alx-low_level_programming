6195777055984380c170158777040e0eccccb25f
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-06 14:41:07 +0300

    solutions for 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - 
+ *
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	char a;
+
+	while (a <= 'z')
+	{
+		if (a != 'e' || a != 'q')
+			putchar(a);
+			a += 1;
+	}
+	putchar('\n');
+	return (0);
+}
