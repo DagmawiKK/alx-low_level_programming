f1ed6d126196a040d186b5031e7e1244e99e5836
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-11 01:40:58 +0300

    solution for 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,17 @@ int print_last_digit(int n)
 	{
 		n = -n;
 		last_digit = n % 10;
-		_putchar(last_digit + '0');
-		return (last_digit);
+		if (last_digit < 0)
+		{
+			last_digit = -last_digit;
+			_putchar(last_digit + '0');
+			return (last_digit);
+		}
+		else
+		{
+			_putchar(last_digit + '0');
+			return (last_digit);
+		}
 	}
 	else
 	{
