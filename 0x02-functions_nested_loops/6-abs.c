bc6a93e8382fd4aca6a8e1cbcab58b9a6e06b960
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-11 01:10:39 +0300

    solution for 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - finds absolute value of an integer
+ *@int: the checked integer
+ *Return: Always 0 (Successful)
+ */
+int _abs(int)
+{
+	if (int >= 0)
+		_putchar(int);
+	else
+		_putchar(int * (-1));
+	return (0);
+}
