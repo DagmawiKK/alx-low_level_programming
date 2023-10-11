146ecac95f659ff3f70866aed9e1762c08cb7fed
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-11 04:10:01 +0300

    solution for print_times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,16 +2,13 @@
 /**
  *times_table - prints times table from 0 to n
  *@n: number of times of the table
- *Return: 0 successful
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int multiple;
+	int i, j, multiple;
 
 	multiple = 0;
-	if (n <= 15 && n >=0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -29,21 +26,19 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(multiple + '0');
-				}
-				else if (multiple >= 10 && multiple < 100)
+				} else if (multiple >= 10 && multiple < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((multiple / 10) + '0');
 					_putchar((multiple % 10) + '0');
-				}
-				else if(multiple >= 100)
+				} else if (multiple >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((multiple / 100) + '0');
-					_putchar((multiple / 10) + '0');
+					_putchar(((multiple / 10) % 10) + '0');
 					_putchar((multiple % 10) + '0');
 				}
 			}
