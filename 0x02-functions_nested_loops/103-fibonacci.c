85de45f72445f38d2bf1fbdfff7bc1bd15f8ed51
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-11 04:44:48 +0300

    solution for 03-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - prints the sum of the even-valued terms of fibonacci
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int term1, term2, sum, next_term;
+
+	next_term = 0;
+	term1 = 1;
+	term2 = 2;
+	sum = 0;
+	while (term1 <= 4000000)
+	{
+		if (term1 % 2 == 0)
+		{
+			sum += term1;
+		}
+		next_term = term1 + term2;
+		term1 = term2;
+		term2 = next_term;
+	}
+	printf("Sum of even-valued terms: %d\n", sum);
+	return (0);
+}
