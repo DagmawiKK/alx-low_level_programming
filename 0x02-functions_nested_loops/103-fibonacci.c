3fbf4b8b959242368d27f6f664cd1e50555a52ed
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-11 04:57:36 +0300

    solution for 104-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,22 +6,39 @@
  */
 int main(void)
 {
-	int term1, term2, sum, next_term;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	next_term = 0;
-	term1 = 1;
-	term2 = 2;
-	sum = 0;
-	while (term1 <= 4000000)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (term1 % 2 == 0)
-		{
-			sum += term1;
-		}
-		next_term = term1 + term2;
-		term1 = term2;
-		term2 = next_term;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
-	printf("%d\n", sum);
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
 	return (0);
 }
+
