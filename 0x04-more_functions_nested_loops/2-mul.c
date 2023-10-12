e73c4fed9cb15a637ae8f56130448d4ff458a661
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-13 02:45:47 +0300

    finding multiple first try

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - multiplies integers
+ *@a: one of the inegers to be multiplied
+ *@b: the other integer
+ *Return: multiple result
+ */
+
+int mul(int a, int b)
+{
+	c = a * b;
+	return (c);
+}
