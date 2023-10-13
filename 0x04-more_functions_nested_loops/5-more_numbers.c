05ef4c524a4ec84874a86283fe1592782bfdf876
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-13 03:40:21 +0300

    answer for printing 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints 0 to 14 10 times
+ *
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
