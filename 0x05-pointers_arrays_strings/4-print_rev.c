9d0e3b3247227a6b5a4ee8a4bf2600a935566b7c
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-17 23:41:15 +0300

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,9 @@
 void print_rev(char *s)
 {
 	int i;
-	int count;
-	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
-		count++;
-	for (j = count - 1; j >= 0; j--)
-		_putchar(s[j]);
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
