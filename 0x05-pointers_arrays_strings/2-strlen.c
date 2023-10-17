635da0c052e2d78319f1023947ae36e92d00e5e4
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-17 22:56:02 +0300

    string size

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*_strlen - returns the length of a string.
+*@s: the length of which the function checks
+*Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+  int count;
+
+  count = 0;
+  for (i = 0; i != '\0'; i++)
+  {
+    *s[i];
+    count++;
+  }
+  return (count);
+}
