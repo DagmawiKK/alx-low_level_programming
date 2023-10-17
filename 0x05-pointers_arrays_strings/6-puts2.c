034ebd9f32ce15542482f1d0c1b812e3349bbf00
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-18 00:16:56 +0300

    prints every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,8 @@
 void puts2(char *str)
 {
 	int i;
-	int count;
-	int j;
 
-	for (j = 0; str[j] != '\0'; j++)
-		count++;
-	for (i = 0; str[i] != '\0' && i < count; i = i + 2)
+	for (i = 0; str[i] != '\0'; i = i + 2)
 		if (str[i] != '0' || str[i] != '\\')
 			_putchar(str[i]);
 	_putchar('\n');
