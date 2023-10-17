7f713ed37c5d024da817a7b1d9c0516ca8fee289
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-17 22:02:25 +0300

    changing value using pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#inlude <stdio.h>
+#include <stdio.h>
 
 void reset_to_98(int *n);
 
-#endif // !MAIN_H
-#define MAIN_H
-
-#inlude <stdio.h>
-
-void reset_to_98(int *n);
+#endif
