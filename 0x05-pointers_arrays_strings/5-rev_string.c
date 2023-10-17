7a5779b2c800b01add794686c7414c2cc980f0f1
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-17 23:58:00 +0300

    print string in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,19 @@
 *@s: string to be reversed
 *Return: void
 */
-
 void rev_string(char *s)
 {
+	char rev = s[0];
+	int counter = 0;
 	int i;
-	int j;
-	int count;
 
-	for (i = 0; s[i] != '\0'; i++)
-		count++;
-	for (j = count - 1; j >= 0; j--)
-	  fputc(s[j], stdout);
-	_putchar('\n');
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
