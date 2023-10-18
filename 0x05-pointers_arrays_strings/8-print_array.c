83196ccc2b8eb2344844366813e462a825bcb84e
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-18 04:08:35 +0300

    prints elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		if (i != n-1)
-			printf("%d, ", a[i]);
-		else
-			 printf("%d", a[i]);
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+			 printf("%d", a[n - 1]);
 	}
 }
