1ce493d5f020000a5165d0355f689b812878a518
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-18 03:45:22 +0300

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+*puts_half - prints half of a string
+*@str: the str to be printed
+*Return: void
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int count;
+	int j;
+	int k;
+	int count2;
+
+	count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	if (count / 2 == 0)
+	{
+		count2 = count / 2;
+		for (j = 0; count2 < count; j++)
+		{
+		_putchar(str[count2]);
+		count2++;
+		}
+	} else
+	{
+		count = count - 1;
+		count2 = count / 2;
+		for (k = 0; count2 < count; k++)
+		{
+			_putchar(str[count2]);
+			count2++;
+		}
+	}
+}
