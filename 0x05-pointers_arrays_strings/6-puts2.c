f370fb80f919777b75dbdcefe2188fdf25caf133
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-18 03:29:01 +0300

    print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,15 +9,15 @@
 void puts2(char *str)
 {
 	int i;
-  int j;
-  int count;
-  char *mem = str;
+	int j;
+	int count;
+	char *mem = str;
 
-  count = 0;
-  for (j = 0; mem[j] != '\0'; j++)
-    count++;
+	count = 0;
+	for (j = 0; mem[j] != '\0'; j++)
+		count++;
 
-  
+	
 	for (i = 0; i <= (count - 1); i++)
 	{
 		if (i % 2 == 0)
