8502bf3c36453d9c15b11e0275120b2b05bd5500
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-20 00:22:56 +0300

    concatenates strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,16 @@
 *@src: the second string.
 *Return: pointer to the result string
 */
-
 char *_strcat(char *dest, char *src)
 {
-	char dest1[];
-	char src1[];
-	int i;
-	int j;
+	int dest_len = 0, i;
 
-	dest1[] = *dest;
-	src1[] = *src;
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
 
-	for (i = 0; dest1[i] != '\0'; i++)
-		return (dest1[i]);
-	for (j = 0; src1[j] != '\0'; j++)
-		return (src1[j]);
+	dest[dest_len + i] = '\0';
+
+	return dest;
 }
