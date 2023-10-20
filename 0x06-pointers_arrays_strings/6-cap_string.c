6ca40012bb0aaf52e7290fb1c919355473a97813
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-21 01:29:35 +0300

    capitalizes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,28 +6,31 @@
 *Return: the capitalized
 */
 
- char *cap_string(char *str)
+char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	int = 0;
+	while (str[i])
 	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
 		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-				i == 0)
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			i == 0)
 		{
-				str[i] = str[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
