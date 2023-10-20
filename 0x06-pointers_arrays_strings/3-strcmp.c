077ec98449a39ce51a5671b4fc22280944978a4b
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-20 18:19:31 +0300

    compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strcmp - compares two strings
+*@s1: one of the strings
+*@s2: the other string
+*Return: 0 if equal, -ve if s1 is less than s2
+*+ve if s1 is greater than s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i:
+	int val;
+
+	for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
+	;
+	val = s1[i] - s2[i];
+
+	return (val);
+}
