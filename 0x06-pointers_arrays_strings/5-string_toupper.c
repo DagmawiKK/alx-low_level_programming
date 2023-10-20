875ceceba9cf5f920b87c818b47887ffc3e9f3bf
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-20 21:31:51 +0300

    uppers sting

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase.
+*@char: pointer to a string
+*Return: converted string
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+			str[i] = str[i] - 32;
+	}
+
+	return (str);
+}
