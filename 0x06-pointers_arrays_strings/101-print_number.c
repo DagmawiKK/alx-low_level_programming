cb34be45e9b3a7d1e1e21e97f489f30b3df49bc3
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-21 01:50:44 +0300

    prints a number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+*print_number - prints an integer
+*@n: integer to be printed
+*Return: void
+*/
+
+void print_number(int n)
+{
+	int divisor;
+	
+	divisor = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) % 10 + '0');
+		divisor /= 10;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+}
+
