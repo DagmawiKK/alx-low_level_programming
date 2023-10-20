6ff9154778b3baeedb67f8dfa246c1d87ff4d845
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-20 21:51:49 +0300

    capitalizes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*cap_string - capitalizes all words of a string
+*@str: to be capitalized
+*Return: the capitalized
+*/
+
+ char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+		{
+				str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
