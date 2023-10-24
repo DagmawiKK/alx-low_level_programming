615a3158c97f0b77a2854b7169b3d143e194abea
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-25 00:15:21 +0300

    writing strcpy

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
 	if (*s == '\0')
 		return (s);
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
+				 s++;
 	}
 	if (c == '\0')
 		return (s);
