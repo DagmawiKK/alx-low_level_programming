422022b24f447d28b3176da237f53800ff2bc8a8
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-24 23:41:39 +0300

    writing memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,8 +12,8 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 		s[i] = b;
-	s[n] = '\0';
+	s[n + 1] = '\0';
 	return (s);
 }
