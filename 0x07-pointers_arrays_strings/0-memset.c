18d0ce2d3e6d46e87c8e849ce94dcecf64b89ce2
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-24 23:36:39 +0300

    writing memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_memset - fills memory with a constant byte
+*@s: points to b
+*@b: the constant byte
+*@n: bytes of memory
+*Return: pointer to memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	s[n] = '\0';
+	return (s);
+}
