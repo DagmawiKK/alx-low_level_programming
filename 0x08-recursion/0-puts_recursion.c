561eb5345367e1b08c6737f90ea0b4eba7c25223
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-26 01:56:45 +0300

    answer for puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (s[i] == '\0')
-  {
-    _putchar('\n');
-    return (NULL);
-  }
-  _putchar(s[i]);
-  _puts_recursion(s[i + 1]);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
