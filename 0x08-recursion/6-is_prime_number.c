4cfb56b3753076d7bf2c9b58c75db25f708b10c2
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-26 02:55:33 +0300

    answer for prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*is_prime_number - checks is a number is prime
+*@n: the checked number
+*Return:	if the input integer is a prime number, otherwise return 0
+*check_prime: checks if a number is prime
+*@i: possible divisor of n
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
+
+int check_prime(int n, int i)
+{
+	if (i >= n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (check_prime(n, i + 1));
+}
