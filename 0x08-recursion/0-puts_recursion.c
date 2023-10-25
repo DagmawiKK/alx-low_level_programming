8efb464c05390a26409f4d773314fba0b856678c
DagmawiKK <dagmawikassa1@gmail.com>
2023-10-26 01:46:23 +0300

    answer for puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints a string, followed by a new line
+*@s: the string to be printed
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] == '\0')
+		return (NULL);
+	_putchar(s[i]);
+	i++;
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+#Answers for project 0x08-recursion
