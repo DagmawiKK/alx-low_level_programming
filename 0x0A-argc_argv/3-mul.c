b16567d9b19fab5986af66c7e7f0222da0f686da
DagmawiKK <dagmawikassa1@gmail.com>
2023-11-02 02:38:45 +0300

    adds two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* main - multiplies two numbers
+* @argc: the number of arguments
+* @argv: pointer to the arguments
+* Return: 0 if successful or else 1
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	char error[] = "Error";
+
+	if (argc != 3)
+	{
+		for (i = 0; error[i] != '\0'; i++)
+			_putchar(error[i]);
+		_putchar('\n');
+		return (1);
+	} else {
+		sum = (int)*argv[1] + (int)*argv[2];
+		_putchar(sum + '0');
+		_putchar('\n');
+		return (0);
+	}
+}
