ec95c463611204c40cc7cac3aad51c178f7415c7
DagmawiKK <dagmawikassa1@gmail.com>
2023-11-02 01:58:26 +0300

    prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main -	prints the number of arguments passed into it
+* @argc: number of arguments passed
+* @argv: pointer to arguments passed
+* Return: Always 0 (Successful)
+*/
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+	_putchar('\n');
+	return (0);
+}
